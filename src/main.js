5d9e30493b446636f9c3055672a051710dca91cf
Kyle Plaugher <kaplaugher94@gmail.com>
2018-11-21 17:36:23 -0500

    global date component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+import AppDate from './components/AppDate.vue';
+
+Vue.component('AppDate', AppDate);
 
 Vue.config.productionTip = false;
 
@@ -9,4 +13,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
