13b7a9c159fe29b34ffb600fc41329f5bd8f8cc9
Kyle Plaugher <kaplaugher94@gmail.com>
2018-11-21 11:27:31 -0500

    add posts

--- src/views/ThreadShow.vue
+++ src/views/ThreadShow.vue
@@ -4,18 +4,21 @@
 
       <h1>{{thread.title}}</h1>
       <PostList :posts="posts" />
+      <PostEditor @save="addPost" :threadId="id" />
 
     </div>
 
   </div>
 </template>
-
+d
 <script>
 import sourceData from '@/data';
 import PostList from '@/components/PostList';
+import PostEditor from '@/components/PostEditor';
 export default {
   components: {
-    PostList
+    PostList,
+    PostEditor
   },
   props: {
     id: {
@@ -34,6 +37,15 @@ export default {
       return Object.values(sourceData.posts).filter(post =>
         postIds.includes(post['.key'])
       );
+    }
+  },
+  methods: {
+    addPost(eventData) {
+      const post = eventData.post;
+      const postId = eventData.post['.key'];
+      this.$set(sourceData.posts, postId, post);
+      this.$set(this.thread.posts, postId, postId);
+      this.$set(sourceData.users[post.userId].posts, postId, postId);
     }
   }
 };
