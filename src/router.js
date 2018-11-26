5b8daad6d26b5a0824bec28f3146948113b289bc
Kaplaugher <kaplaugher94@gmail.com>
2018-11-25 20:04:28 -0500

    forum logic

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Home from '@/views/Home.vue';
 import ThreadShow from '@/views/ThreadShow.vue';
 import PageNotFound from '@/views/PageNotFound.vue';
+import Forum from '@/views/PageForum.vue';
 
 Vue.use(Router);
 
@@ -14,6 +15,12 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/forum/:id',
+      name: 'Forum',
+      component: Forum,
+      props: true
     },
     {
       path: '/about',
